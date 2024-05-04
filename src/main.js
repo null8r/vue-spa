2ea9a6222d7df709a0a9136b6e965df6f79d831f
null <91502871+null8R@users.noreply.github.com>
2024-05-04 17:27:33 +0900

    ファイル整理

--- src/main.js
+++ src/main.js
@@ -1,14 +1,4 @@
-const routes = [
-  { path: "/", component: httpVueLoader("./src/components/index.vue") },
-  { path: "/page1", component: httpVueLoader("./src/components/page1.vue") },
-  { path: "/page2", component: httpVueLoader("./src/components/page2.vue") }
-];
-
-const router = VueRouter.createRouter({
-  history: VueRouter.createWebHashHistory(), //URLに/#/が入る(URLのコピーして共有が可・SEO不利) example.com/#/home
-  //history: VueRouter.createWebHistory(), //URL/#/が入らないが、直接アクセスすると404になる(URLのコピーして共有が不可) example.com/home
-  routes
-});
+import { router } from "./router.js"
 
 const app = Vue.createApp({});
 
